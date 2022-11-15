d87d24526d1ed91401f56df9576f8e229d23588b
danyloandreev <danyloandreiev@gmail.com>
2022-11-15 12:19:13 +0100

    TASK-1 schedule hourly

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -16,7 +16,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('hour:get_temperature')
-            ->everyMinute();
+            ->hourly();
     }
 
     /**
